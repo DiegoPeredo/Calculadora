84773b1ed0c23aece9fe49ab0a69428fb54f6508
Diego Peredo <a2019118858@estudiantes.upsa.edu.bo>
2021-02-25 20:08:22 -0400

    Se creó la solución Calculadora (Funcional)

---
+++ SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿namespace ApiCalculadora.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost]
+        [Route ("Add")]
+        public float Add([FromHeader] float numa, [FromHeader] float numb)
+        {
+            return numa + numb;
+        }
+        [HttpPost]
+        [Route("Multi")]
+        public float Multi([FromHeader] float numa, [FromHeader] float numb)
+        {
+            return numa * numb;
+        }
+        [HttpPost]
+        [Route("Divide")]
+        public float Divide([FromHeader] float numa, [FromHeader] float numb)
+        {
+            return (numa / numb);
+        }
+        [HttpPost]
+        [Route("Substract")]
+        public float Substract([FromHeader] float numa, [FromHeader] float numb)
+        {
+            return numa - numb;
+        }
+    }
+}
