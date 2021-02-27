fe1441d47e09629c327e091ccfabc50a8874982a
Diego Peredo <a2019118858@estudiantes.upsa.edu.bo>
2021-02-27 10:01:14 -0400

    Se agregó Get de suma, resta, multiplicacion y division

--- SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
+++ SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
@@ -12,23 +12,59 @@
         {
             return numa + numb;
         }
+
+        [HttpGet]
+        [Route("Add")]
+        public float Add2( float numa, float numb)
+        {
+            return numa + numb;
+        }
+
         [HttpPost]
         [Route("Multi")]
         public float Multi([FromHeader] float numa, [FromHeader] float numb)
         {
             return numa * numb;
         }
+
+        [HttpGet]
+        [Route("Multi")]
+        public float Multi2(float numa, float numb)
+        {
+            return numa * numb;
+        }
+
         [HttpPost]
         [Route("Divide")]
         public float Divide([FromHeader] float numa, [FromHeader] float numb)
         {
+            if (numb == 0)
+                return 0;
             return (numa / numb);
         }
+
+        [HttpGet]
+        [Route("Divide")]
+        public float Divide2(float numa, float numb)
+        {
+            if (numb == 0)
+                return 0;
+            return (numa / numb);
+        }
+
         [HttpPost]
-        [Route("Substract")]
-        public float Substract([FromHeader] float numa, [FromHeader] float numb)
+        [Route("Subtract")]
+        public float Subtract([FromHeader] float numa, [FromHeader] float numb)
         {
             return numa - numb;
         }
+
+        [HttpGet]
+        [Route("Subtract")]
+        public float Subtract2(float numa, float numb)
+        {
+            return numa - numb;
+        }
+
     }
 }
